b8b8bf7dd358d303ec24f614fe3381992b1c1c11
Filip Jovakaric <filip.jovakaric@gmail.com>
2019-02-27 14:38:44 +0100

    adding address generator

--- src/utils/address-generator.js
+++ src/utils/address-generator.js
@@ -1,7 +1,34 @@
-const wip = require('wip')
+const bip38 = require('bip38')
 const bitcoin = require('bitcoinjs-lib')
+const coininfo = require('coininfo')
+const coinkey = require('coinkey')
 
-const keyPair = bitcoin.ECPair.makeRandom()
-const { publicKey, privateKey } = keyPair
+const supportedCoins = [
+    'BCH',
+    'BTC',
+    'BTG',
+    'DASH',
+    'DCR',
+    'DOGE',
+    'LTC',
+    'ZEC',
+]
 
-console.log({ publicKey, privateKey })
+const addressGenerator = {
+    generateKeyPair,
+}
+
+function generateKeyPair(passphrase, coin = 'BTC') {
+    if (!supportedCoins.includes(coin)) return console.log('Wrong coin')
+
+    const keyPair = coinkey.createRandom(coininfo(coin));
+    const {privateKey} = keyPair
+
+    return {
+        publicKey: keyPair.publicAddress,
+        privateKey: bip38.encrypt(privateKey, true, passphrase),
+        coin,
+    }
+}
+
+// supportedCoins.forEach((coin) => console.log(generateKeyPair('Random pass', coin)))
