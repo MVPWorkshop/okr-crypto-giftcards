d1caa086326d730d64fff7089a3f0e8c503a70a9
Srdjan Djenader <djenka76@gmail.com>
2019-02-28 12:04:54 +0100

    preview content added

--- src/components/Preview/index.vue
+++ src/components/Preview/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="card">
-    <h1>Preview</h1>
-    <h2>Front Side</h2>
+  <div class="card giftcard-preview">
+    <h1 class="giftcard-heading">Preview</h1>
+    <h2 class="giftcard-side">Front Side</h2>
 
     <div></div>
 
-    <h2>Back Side</h2>
-    <p>
-      <strong>Note: {{form.forWhom}}</strong>...
-    </p>
+    <h2 class="giftcard-side">Back Side</h2>
+    <p><strong>Note: </strong>You should handwrite some special note (Passphrase) you wrote previosly on the giftcard after you receive it or print it. Whatch out for capital letters and interpunction signs.</p>
   </div>
 </template>
 
@@ -26,4 +24,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+  .giftcard-side,
+  .giftcard-heading {
+    color: #C5C4DF;
+    text-align: center;
+  }
+
+  .giftcard-heading {
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-bottom: 1rem;
+  }
+
+  .giftcard-side {
+    font-size: 1.2rem;
+  }
+
+  .giftcard-preview {
+    background-color: #F8FAFF;
+    border-color: #E4E4F7;
+  }
 </style>
